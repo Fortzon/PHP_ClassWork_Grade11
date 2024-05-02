0b43b9425cac3c3216f37912375a4289f3453586
MiguelCViegas <al_18361@aegp.edu.pt>
2024-05-02 15:35:20 +0100

    Update ex1.php

--- ex1.php
+++ ex1.php
@@ -1,5 +1,35 @@
-<?php 
-echo "Hello World";
-echo "<br>";
+<?php
+/*echo "Hello World";
+echo "<br>";*/
 echo "<h1> Hello World!</h1>";
+//Fixe
+
+$Nome = "Miguel";
+$Idade = 16;
+$casado = false;
+$altura = 1.64;
+
+
+
+//Constantes
+const pi = 3.1416;
+
+define("Config", "file.cfg");
+//Constantes Mágicas
+echo __FILE__;
+echo __DIR__;
+echo __LINE__;
+
+
+//var_dump
+echo "<br>";
+var_dump($Nome);
+echo "<br>";
+var_dump($altura);
+
+
+//Atribuição encadeada
+$n1=$n2=$n3=300;
+
+echo "Olá $nome! Tens $idade e medes $altura";
 ?>
