c22cc8270e8a048480d4094ee9b6176c03724f38
MiguelCViegas <al_18361@aegp.edu.pt>
2024-05-02 17:09:45 +0100

    aula 02/05

--- ex1.php
+++ ex1.php
@@ -7,8 +7,7 @@ echo "<h1> Hello World!</h1>";
 $Nome = "Miguel";
 $Idade = 16;
 $casado = false;
-$altura = 1.64;
-$Dinheiro = 1000;
+$Altura = 1.64;
 
 
 
@@ -26,19 +25,69 @@ echo __LINE__;
 echo "<br>";
 var_dump($Nome);
 echo "<br>";
-var_dump($altura);
+var_dump($Altura);
 
 
 //Atribuição encadeada
-$n1=$n2=$n3=300;
+$Dinheiro=$n2=$n3=300;
 
-echo "Olá $nome! Tens $Idade e medes $Altura m.";
+echo "<br>";
+
+echo "Olá $Nome! Tens $Idade anos e medes $Altura m.";
+
+echo "<br>";
 
 echo "Olá $Nome! Tens a pagar $Dinheiro €";
+
+echo "<br>";
 
 $soma = "2"+"2";
 
 echo $soma;
 
 var_dump($soma);
+
+//Strings
+
+$seasons = "Primavera, Verão, Outono, Inverno";
+echo "<br>";
+echo $seasons;
+
+echo $seasons[9];
+
+echo "<br>";
+
+echo strlen($seasons);
+
+echo "<br>";
+
+echo substr($seasons,11,6);
+
+echo "<br>";
+
+echo strtoupper($seasons);
+
+echo "<br>";
+
+echo str_contains($seasons, "Pedro");
+
+echo str_replace( "Verão", "Fortnite",$seasons);
+
+echo "<br>";
+
+echo $seasons;
+
+$NomeDoFilme = (string)$n2;
+
+//Operadores
+// ** Potencia
+//+ - * / %
+//++ --
+// += -= *= /=
+//Usa o PEMDAS (Potencia, expoente, multiplicação, adição, subtração)
+
+//Funções
+echo "<br>";
+echo base_convert("AB39C10",12,8);
+
 ?>
---
+++ form1.php
@@ -0,0 +1,7 @@
+<?php 
+$Util = $_REQUEST["utilizador"];
+$valor = $_REQUEST["valor"];
+$fun = $_REQUEST["funcao"];
+
+echo "Olá $Util a sua função é $fun e recebe $valor euros";
+?>
